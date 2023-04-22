4882def0c7f870f5659e2ed6efe4fd241a436769
Your Name <you@example.com>
2023-04-22 14:16:17 -0700

    l

--- printf_0.c
+++ printf_0.c
@@ -1,4 +1,11 @@
 #include"main.h"
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * @...: optional arguments
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	const char *s;
@@ -31,6 +38,14 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (size);
 }
+/**
+ * switches - helper function for handling conversion specifiers in printf
+ * @args: a va_list containing the arguments to be printed
+ * @c: the conversion specifier to use
+ * @size: a pointer to the size of the output string
+ *
+ * Return: a string representing the converted value
+ */
 int switches(va_list args,char c, size_t *size)
 {
 	char  *str;
@@ -58,6 +73,7 @@ int switches(va_list args,char c, size_t *size)
 			write(1, str, strlen(str));
 			*size += count_chars(x);
 			return (2);
+			break;
 		case '%':
 			x = '%';
 			write(1, &x, 1);
