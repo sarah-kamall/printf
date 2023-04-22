c437a4b0e72a20c9714bae4badcf6d1423919a7f
Your Name <you@example.com>
2023-04-22 13:43:15 -0700

    try baa

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include"main.h"
 int main()
 {
-	x = _printf("STRINGS %s and CHARS %c and INTEGERS  %i \n", "HELLO\0", 'l', 1234);
+	_printf("STRINGS %s and CHARS %c and INTEGERS  %i \n", "HELLO\0", 'l', 1234);
 	return (0);
 }
